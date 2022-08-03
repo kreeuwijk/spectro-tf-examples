4a22beb170be40f4464e1119651742653db8ef20
Kevin Reeuwijk <kevin.reeuwijk@spectrocloud.com>
2022-08-03 19:32:30 +0200

    enable both EBS and EFS

--- eks_with_efs_csi/locals.tf
+++ eks_with_efs_csi/locals.tf
@@ -1,5 +1,5 @@
 data "spectrocloud_cluster_profile" "dev-eks" {
-  # This profile needs to contain the "csi-aws-ebs" and "csi-aws-efs-add-on" packs
+  # This profile needs to contain the "csi-aws-ebs" v1.8.0 and "csi-aws-efs-add-on" v1.4.0 packs
   name    = "Dev-EKS"
   version = "1.0.0"
 }
@@ -10,9 +10,16 @@ locals {
       id            = data.spectrocloud_cluster_profile.dev-eks.id
       packs         = [
         {
+          name = "csi-aws-ebs"
+          tag  = "1.8.0"
+          values = templatefile("./config/csi-aws-ebs.yaml", {
+            ebs-role-arn : "arn:aws:iam::${var.aws_account_number}:role/EBS-CSI-Driver-Role-${var.sc_eks_cluster_name}"
+          })
+        },
+        {
           name = "csi-aws-efs-add-on"
           tag  = "1.4.0"
-          values = templatefile("./config/efs.yaml", {
+          values = templatefile("./config/csi-aws-efs.yaml", {
             fs-id        : aws_efs_file_system.efs.id,
             efs-role-arn : "arn:aws:iam::${var.aws_account_number}:role/EFS-CSI-Driver-Role-${var.sc_eks_cluster_name}"
           })
