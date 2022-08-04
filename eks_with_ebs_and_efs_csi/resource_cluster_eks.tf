b3b3a2ec0b3e7dfc2417a822bd73fea30063e25a
Kevin Reeuwijk <kevin.reeuwijk@spectrocloud.com>
2022-08-04 11:58:55 +0200

    update worker pool count to 2

--- eks_with_ebs_and_efs_csi/resource_cluster_eks.tf
+++ eks_with_ebs_and_efs_csi/resource_cluster_eks.tf
@@ -30,7 +30,7 @@ resource "spectrocloud_cluster_eks" "cluster" {
   machine_pool {
     name          = "worker-pool"
     min           = 1
-    count         = 1
+    count         = 2
     max           = 3
     instance_type = "t3.large"
     disk_size_gb  = 60
